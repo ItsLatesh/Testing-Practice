a667c1f1dba2fcb2114f4234d01700f7da0448f9
Latesh Ghangal <ghangallatesh2001@gmail.com>
2025-05-15 16:46:12 +0530

    updated

--- src/test/java/keyboardActions/KeyboardActions.java
+++ src/test/java/keyboardActions/KeyboardActions.java
@@ -1,5 +1,9 @@
 package keyboardActions;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -11,19 +15,47 @@ public class KeyboardActions {
 	public static void main(String[] args) {
 		// keyboard actions - actions 
 		/*
-		 * */
+		 * keydown()
+		 * keyup()
+		 * for three keys for example
+		 * ctrl+shift+A
+		 * act.keydown(keys.control).keydown(keys.shift).sendkeys("A").keyup(keys.shift).keyup(keys.control).perform*/
 		
        WebDriverManager.chromedriver().setup();
 		
 		
 		WebDriver driver = new ChromeDriver();
+		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		        
 		
 		
-	            driver.get("https://www.jqueryscript.net/demo/Price-Range-Slider-jQuery-UI/");
+	            driver.get("https://text-compare.com/");
 	            driver.manage().window().maximize();
+	            
+	            driver.findElement(By.xpath("//textarea[@id='inputText1']")).sendKeys("WELCOME");
+	            
 	            
 	            Actions act = new Actions(driver);  
 	            
+	            //CTRL+A - SELECTS THE TEXT
+	            
+	            act.keyDown(Keys.CONTROL).sendKeys("A").keyUp(Keys.CONTROL).perform();
+	            
+	            
+	            //CTRL+C - COPY THE TEXT INTO CLIPBORAD
+	            
+	            act.keyDown(Keys.CONTROL).sendKeys("C").keyUp(Keys.CONTROL).perform();
+	            
+	            
+	            //TAB - SHIFT TO 2ND BOX
+	            
+	            act.keyDown(Keys.TAB).keyUp(Keys.TAB).perform();
+	            
+	            
+	            //CTRL+V - PASTE THE TEXT
+	            
+	            
+	            act.keyDown(Keys.CONTROL).sendKeys("V").keyUp(Keys.CONTROL).perform();
 
 	}
 
