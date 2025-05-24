1a9a9054595b7cfc01739dc3edc29d4ed27777b3
Latesh Ghangal <ghangallatesh2001@gmail.com>
2025-05-24 15:51:44 +0530

    ssl handling

---
+++ src/test/java/sslhandlingexm/SSLhandlingExm.java
@@ -0,0 +1,30 @@
+package sslhandlingexm;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SSLhandlingExm {
+
+	public static void main(String[] args) {
+		/*Secure Socket Layer handling 
+		 * 
+		 * */
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions options = new ChromeOptions();
+		
+		options.setAcceptInsecureCerts(true);//accepts SSL certificates 
+
+		WebDriver driver = new ChromeDriver(options );
+		
+		
+	        driver.get("https://expired.badssl.com/");
+	        
+	        System.out.println("title of the page: "+driver.getTitle());//privacy error
+	        
+	}
+
+}
